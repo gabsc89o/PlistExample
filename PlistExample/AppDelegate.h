dc81121be1b9dd254f03250f869d834fefd2d6b6
gabsc89o <user138066@eu249.local>
2018-04-20 14:33:40 +0100

    Initial Commit

---
+++ PlistExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlistExample
+//
+//  Created by user138066 on 4/20/18.
+//  Copyright © 2018 user138066. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
